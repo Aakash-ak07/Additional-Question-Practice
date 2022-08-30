c6dba41a4269df9ff1cf3cc7ee201bc15025061e
Aakash-ak07 <111000108+Aakash-ak07@users.noreply.github.com>
2022-08-30 16:50:55 +0530

    Create Simple Input- String.java

---
+++ Variables & Operators/Simple Input- String.java
@@ -0,0 +1,15 @@
+//Given a string S, your task is to take input of the given string and print the same string
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner S= new Scanner(System.in);
+        String name = S.next();
+
+        System.out.println(name);
+        
+    }
+}
