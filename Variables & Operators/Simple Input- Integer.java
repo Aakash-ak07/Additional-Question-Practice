53d8fe2d444a2e5d289828d3462d413c554834fd
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-08-31 18:15:56 +0530

    Create Simple Input- Integer.java

---
+++ Variables & Operators/Simple Input- Integer.java
@@ -0,0 +1,21 @@
+//Given an integer N, your task is to take input of the given integer and print it.
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.println(n);
+    }
+}
+
+
+// Sample Input:-
+// 56
+
+// Sample Output:-
+// 56
