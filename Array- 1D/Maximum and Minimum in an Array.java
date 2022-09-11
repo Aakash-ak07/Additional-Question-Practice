0f3dce494c357540653c5fceb44abd1e176dea40
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-11 18:28:28 +0530

    Create Maximum and Minimum in an Array.java

---
+++ Array- 1D/Maximum and Minimum in an Array.java
@@ -0,0 +1,55 @@
+// Given an array A[ ] of size N containing positive integers, find maximum and minimum elements from the array.
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static int getMax(int[] arr, int n)
+    {
+        int Max = arr[0];
+        for(int i = 0; i < n; i++)
+        {
+            Max = Math.max(Max,arr[i]);
+        }
+        return Max; 
+    }
+    public static int getMin (int[] arr, int n)
+    {
+    int Min = arr[0];
+        for(int i = 0; i < n; i++)
+        {
+            Min = Math.min(Min,arr[i]);
+        }
+        return Min;
+    }
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int k = 0; k < t; k++)
+        {
+            int n = sc.nextInt();
+            int [] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            System.out.print(getMax(arr, n)+" ");
+            System.out.print(getMin(arr, n));
+            System.out.println();
+        }
+    }
+}
+
+// Sample Input:
+// 2
+// 5
+// 7 3 4 5 6
+// 4
+// 1 2 3 4
+
+// Sample Output:
+// 7 3
+// 4 1
