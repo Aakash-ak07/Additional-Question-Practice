3574bdb05fa532ce20e8f790eda10508df31a874
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-10-21 19:30:03 +0530

    Maximum and Minimum in an Array

--- Array- 1D/Maximum and Minimum in an Array.java
+++ Array- 1D/Maximum and Minimum in an Array.java
@@ -53,3 +53,40 @@ class Main {
 // Sample Output:
 // 7 3
 // 4 1
+
+-----------------
+//2nd solution
+-----------------
+    
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int min=Integer.MAX_VALUE;
+            int max=Integer.MIN_VALUE;
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = sc.nextInt();
+                if(arr[i] < min)
+                {
+                    min = arr[i];
+                }
+                if(arr[i] > max)
+                    max = arr[i];
+            }
+            // Arrays.sort(arr);
+            System.out.print(max + " " + min);
+            System.out.println("");
+        }
+    }
+}
